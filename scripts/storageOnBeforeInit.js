464f8dc5d3f9d7497ee61e7c790a65805cdc5301
DmytroZubelevych <31444413+DmytroZubelevych@users.noreply.github.com>
2022-02-18 11:28:27 +0200

    Update storageOnBeforeInit.js

--- scripts/storageOnBeforeInit.js
+++ scripts/storageOnBeforeInit.js
@@ -8,8 +8,11 @@ var hasCollaboration = (parseInt('${fn.compareEngine(7.0)}', 10) >= 0),
     q = [];
 
 if (hasCollaboration) {
-    quotas = JSON.parse('${quota.data}');
-    quotas = [ quotas[STORAGE_NODE_DISK_LIMITATION], quotas[SAME_NODES], quotas[MAX_NODES] ];
+    quotas = [
+        { quota : { name: MAX_NODES }, value: parseInt('${quota.environment.maxnodescount}', 10) },
+        { quota : { name: SAME_NODES }, value: parseInt('${quota.environment.maxsamenodescount}', 10) },
+        { quota : { name: STORAGE_NODE_DISK_LIMITATION }, value: parseInt('${quota.storage.node.disk.limitation}', 10) }
+    ];
 } else {
     quotas = jelastic.billing.account.GetQuotas(STORAGE_NODE_DISK_LIMITATION + ";" + SAME_NODES + ";" + MAX_NODES).array || [];
 }
