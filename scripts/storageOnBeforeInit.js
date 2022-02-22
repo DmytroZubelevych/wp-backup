6b0864f33bfa5432c7d744c25c011dbeb3d54bec
DmytroZubelevych <31444413+DmytroZubelevych@users.noreply.github.com>
2022-02-22 12:17:32 +0200

    Update storageOnBeforeInit.js

--- scripts/storageOnBeforeInit.js
+++ scripts/storageOnBeforeInit.js
@@ -39,7 +39,7 @@ return {
         "default": "1",
         "values": {
           "1": "Standalone",
-          "2": "Cluster (GlusterFS) nodes"
+          "2": "Cluster"
         },
         "showIf": {
           "2": [
